2b5510acb1f124b1fb16bd25c4e05f1227bbef48
mjok <marijajoksimovic@hotmail.com>
2018-05-26 23:11:19 +0200

    Postavljanje projekata.

---
+++ XWS_2018_Backend/src/main/java/com/ftn/WebXML2018/XWS_2018_Backend/Xws2018BackendApplication.java
@@ -0,0 +1,12 @@
+package com.ftn.WebXML2018.XWS_2018_Backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Xws2018BackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Xws2018BackendApplication.class, args);
+	}
+}
---
+++ XWS_2018_Backend/src/test/java/com/ftn/WebXML2018/XWS_2018_Backend/Xws2018BackendApplicationTests.java
@@ -0,0 +1,16 @@
+package com.ftn.WebXML2018.XWS_2018_Backend;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Xws2018BackendApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}

15021575a824ad1d7998abe8c733f173137eefde
milosrs <milosribar@yahoo.com>
2018-06-21 23:55:32 +0200

    Web servis pocetak

---
+++ XWS_2018_Backend/src/main/java/com/ftn/WebXML2018/XWS_2018_Backend/config/WebServiceConfig.java
@@ -0,0 +1,28 @@
+package com.ftn.WebXML2018.XWS_2018_Backend.config;
+
+import org.apache.cxf.Bus;
+import org.apache.cxf.jaxws.EndpointImpl;
+
+import javax.xml.ws.Endpoint;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.ftn.WebXML2018.XWS_2018_Backend.service.AgentEndpointService;
+
+@Configuration
+public class WebServiceConfig {
+
+	@Autowired
+	private Bus bus;
+	
+	@Autowired
+	private AgentEndpointService agentEndpointService;
+	
+	@Bean
+    public Endpoint endpoint() {
+        EndpointImpl endpoint = new EndpointImpl(bus, agentEndpointService);
+        endpoint.publish("/agentEndpointService");  // putanja_do_spring_boot_aplikacije + putanja_do_cxf_servleta + endpoint_path
+        return endpoint;                            // putanja_do_cxf_servleta definisana u application.properties 
+    }
+}

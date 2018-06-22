b34a0abf2d93add347edac5583c54d5a5ab2bd02
timotej-orcic <timotej.orcic@gmail.com>
2018-06-22 15:53:43 +0200

    Rad na Agentskim servisima

--- XWS_2018_Backend/src/main/java/com/ftn/WebXML2018/XWS_2018_Backend/serviceImpl/AgentEndpointServiceImpl.java
+++ XWS_2018_Backend/src/main/java/com/ftn/WebXML2018/XWS_2018_Backend/serviceImpl/AgentEndpointServiceImpl.java
@@ -1,15 +1,39 @@
 package com.ftn.WebXML2018.XWS_2018_Backend.serviceImpl;
 
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Base64;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Optional;
+import java.util.Set;
+
 import javax.jws.WebMethod;
 import javax.jws.WebService;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.ftn.WebXML2018.XWS_2018_Backend.agentDto.BookingUnit_DTO;
+import com.ftn.WebXML2018.XWS_2018_Backend.dto.AgentUserDTO;
+import com.ftn.WebXML2018.XWS_2018_Backend.entity.AccomodationCategory;
+import com.ftn.WebXML2018.XWS_2018_Backend.entity.AccomodationType;
+import com.ftn.WebXML2018.XWS_2018_Backend.entity.AgentUser;
+import com.ftn.WebXML2018.XWS_2018_Backend.entity.BonusFeatures;
+import com.ftn.WebXML2018.XWS_2018_Backend.entity.BookingUnit;
+import com.ftn.WebXML2018.XWS_2018_Backend.entity.BookingUnitPicture;
+import com.ftn.WebXML2018.XWS_2018_Backend.entity.City;
 import com.ftn.WebXML2018.XWS_2018_Backend.repository.BookingUnitRepository;
 import com.ftn.WebXML2018.XWS_2018_Backend.responseWrapper.ResponseWrapper;
+import com.ftn.WebXML2018.XWS_2018_Backend.service.AccomodationCategoryService;
+import com.ftn.WebXML2018.XWS_2018_Backend.service.AccomodationTypeService;
 import com.ftn.WebXML2018.XWS_2018_Backend.service.AgentEndpointService;
+import com.ftn.WebXML2018.XWS_2018_Backend.service.AgentUserService;
+import com.ftn.WebXML2018.XWS_2018_Backend.service.BonusFeaturesService;
 import com.ftn.WebXML2018.XWS_2018_Backend.service.BookingUnitService;
+import com.ftn.WebXML2018.XWS_2018_Backend.service.CityService;
 import com.ftn.WebXML2018.XWS_2018_Backend.service.MessageService;
 import com.ftn.WebXML2018.XWS_2018_Backend.service.MonthlyPricesService;
 import com.ftn.WebXML2018.XWS_2018_Backend.service.ReservationService;
@@ -35,18 +59,96 @@ public class AgentEndpointServiceImpl implements AgentEndpointService{
 	@Autowired
 	private UserService userService;
 	
+	@Autowired
+	private CityService cityService;
+	
+	@Autowired
+	private AccomodationTypeService accTypeService;
+	
+	@Autowired
+	private AccomodationCategoryService accCatService;
+	
+	@Autowired
+	private AgentUserService agentUserService;
+	
+	@Autowired
+	private BonusFeaturesService bFeaturesService;
+	
 	@Override
 	@WebMethod
-	public ResponseWrapper<?> agentLogin() {
+	public String agentLogin() {
 		// TODO Auto-generated method stub
-		return new ResponseWrapper<String>("Success Timeeeeh!", "Success!", true);
+		return "Success Timeeeeh!";
 	}
 
 	@Override
 	@WebMethod
-	public ResponseWrapper<?> addBookingUnit() {
-		// TODO Auto-generated method stub
-		return null;
+	public ResponseWrapper<?> addBookingUnit(BookingUnit_DTO buDto) {
+		ResponseWrapper<?> retObj = new ResponseWrapper<String>();
+		
+		City city = cityService.getOne(buDto.getCityMainServerId());
+		if(city == null) {
+			retObj.setSuccess(false);
+			retObj.setMessage("Invalid city input. Please try again.");
+			return retObj;
+		}
+		
+		AgentUser agentUsr = (AgentUser)userService.getUser(buDto.getAgentMainServerId());
+		if(agentUsr == null) {
+			retObj.setSuccess(false);
+			retObj.setMessage("Can't find agent data on main server. Please try again.");
+			return retObj;
+		}
+		
+		AccomodationType accType = accTypeService.getById(buDto.getAccomodationTypeMainServerId());
+		if(accType == null) {
+			retObj.setSuccess(false);
+			retObj.setMessage("Can't find the given accomodation type data on main server. Please try again.");
+			return retObj;
+		}
+		
+		AccomodationCategory accCat = accCatService.getById(buDto.getAccomodationCategoryMainServerId());
+		if(accCat == null) {
+			retObj.setSuccess(false);
+			retObj.setMessage("Can't find the given accomodation category data on main server. Please try again.");
+			return retObj;
+		}
+		
+		Set<BonusFeatures> bonusFeatures;
+		for(Iterator<Long> i = buDto.getBonusFeaturesMainServerIds().iterator(); i.hasNext();) {
+			Long bfId = i.next();
+			BonusFeatures bFeature = bFeaturesService.getById(bfId);
+			if(bFeature == null) {
+				retObj.setSuccess(false);
+				retObj.setMessage("Can't find the given bonus feature data on main server. Please try again.");
+				return retObj;
+			}
+			bonusFeatures.add(bFeature);
+		}
+		
+		BookingUnit unit = new BookingUnit(buDto.getAddress(), buDto.getName(), buDto.getDescription(), 
+				buDto.getPeopleNo(), city, agentUsr, 
+				bonusFeatures, accType, 
+				accCat, null);
+		
+		//bookingUnitService.
+		
+		Set<BookingUnitPicture> pictures;
+		for (Map.Entry<String, String> item : buDto.getBase64ImagesList().entrySet()) {
+		    String base64Img = item.getValue();
+		    String imgName = item.getKey();
+		   
+		    byte[] data = Base64.getDecoder().decode(base64Img);
+		    //dodaj timestamp u ime slike
+		    
+			Path destinationFile = Paths.get("/images", imgName);
+			Files.write(destinationFile, data);
+			
+			BookingUnitPicture myPicture = new BookingUnitPicture(imgName, null);
+			pictures.add(myPicture);
+		}	
+		
+		return retObj;
 	}
 
 	@Override
